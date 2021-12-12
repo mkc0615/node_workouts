f6cfa1afed0fa93a55a72a02bd9789d1c7cd9899
Min Kee Choi <mkc0615@gmail.com>
2021-12-12 15:31:05 +0900

    Added security measures by keys and gitignore

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const bodyParser = require('body-parser')
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+// application/json
+app.use(bodyParser.json())
+
+const {User} = require('./models/User')
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://minkee:cjsrnr0925@workout.rma94.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log("MongoDB connected!"))
 .catch(err => console.log(err))
 
-app.get('/', (req, res)=>res.send('Hello World'))
+app.get('/', (req, res)=>res.send('Hello World! Good Luck!'))
+
+app.post('/register', (req, res) => {
+    // Registration info
+    const user = new User(req.body)
+
+    user.save((err, doc)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
